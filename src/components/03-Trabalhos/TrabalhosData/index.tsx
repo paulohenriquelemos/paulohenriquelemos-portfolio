2433905e7e2ba4b63927b648db44cf76dcfccf0e
Paulo Henrique de Souza Lemos <ph-lemos@hotmail.com>
2023-04-24 17:40:34 -0300

    change path image

--- src/components/03-Trabalhos/TrabalhosData/index.tsx
+++ src/components/03-Trabalhos/TrabalhosData/index.tsx
@@ -1,10 +1,12 @@
+import imageBlogIsca from '../../../assets/Blog-Isca-e-Cia.png'
+
 export const trabalhosData = [
   {
     id: '001',
     title: 'Blog Isca e Companhia',
     description:
       'O projeto consistiu em migrar o site existente em html para ReactJS, utilizando o mínimo possível de plugins do WordPress. Foi bastante desafiador, mas muito gratificante finalizar o front-end e ajudar com as requisições via API do WordPress.',
-    image: '/src/assets/Blog-Isca-e-Cia.png',
+    image: imageBlogIsca,
     link: 'https://blogiscaecompanhia.com.br',
     topics: ['React', 'WordPress', 'API'],
     github: '',
